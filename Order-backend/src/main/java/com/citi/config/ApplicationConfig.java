c7fae74a901b28f81a78d268495b9a9025935cfa
srinivaschakravarthy <srinivaschakravarthy1996@gmail.com>
2017-09-28 14:04:47 +0530

    First commit

---
+++ Order-backend/src/main/java/com/citi/config/ApplicationConfig.java
@@ -0,0 +1,27 @@
+package com.citi.config;
+
+import javax.inject.Named;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ApplicationConfig {
+
+@Named
+static class JerseyConfig extends ResourceConfig {
+public JerseyConfig() {
+	this.packages("com.citi.rest");}
+}
+
+
+@Bean
+public RestTemplate restTemplate() {
+RestTemplate restTemplate = new RestTemplate();
+
+return restTemplate;
+}
+
+}
---
+++ Product-backend/src/main/java/com/citi/config/ApplicationConfig.java
@@ -0,0 +1,17 @@
+package com.citi.config;
+
+import javax.inject.Named;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+
+@Named
+static class JerseyConfig extends ResourceConfig {
+public JerseyConfig() {
+	this.packages("com.citi.rest");}
+}
+
+}
